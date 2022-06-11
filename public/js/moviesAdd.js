d0d1d7edc57532f3011d694b354124cead2a3290
HawkDiego <giraldito99@gmail.com>
2022-06-10 23:55:08 -0500

    Add styles from js to moviesAdd

---
+++ public/js/moviesAdd.js
@@ -0,0 +1,11 @@
+let h1 = document.querySelector("h1");
+let section = document.querySelector("section");
+let article = document.querySelector("article");
+
+console.log(article, h1, section);
+
+h1.innerHTML = "AGREGAR PELÍCULAS";
+h1.classList.add("titulo");
+
+article.classList.add("fondoTransparente");
+section.classList.add("fondoaCRUD");
